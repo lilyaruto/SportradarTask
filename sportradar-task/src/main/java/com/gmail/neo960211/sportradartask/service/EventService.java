b29eda67e2c3ea1e6cbef32078241d7e028491f2
Yaroslav <ne0960211@gmail.com>
2023-01-03 14:08:40 +0100

    Method to sort events by probability

--- sportradar-task/src/main/java/com/gmail/neo960211/sportradartask/service/EventService.java
+++ sportradar-task/src/main/java/com/gmail/neo960211/sportradartask/service/EventService.java
@@ -4,6 +4,8 @@ import com.gmail.neo960211.sportradartask.model.Event;
 import com.gmail.neo960211.sportradartask.repository.EventRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -16,7 +18,30 @@ public class EventService {
 
     public List<Event> sortList() {
         List<Event> list = (List<Event>) eventRepository.findAll();
+        list.sort(new Comparator<Event>() {
+            @Override
+            public int compare(Event e1, Event e2) {
+                if (compareProbabilities(e1) < compareProbabilities(e2)) {
+                    return 1;
+                } else if (compareProbabilities(e1) > compareProbabilities(e2)) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
         return list;
+    }
+
+    public static double compareProbabilities(Event e) {
+        if (e.getProbability_home_team_winner() > e.getProbability_away_team_winner()
+                && e.getProbability_home_team_winner() > e.getProbability_draw()) {
+            return e.getProbability_home_team_winner();
+        } else if (e.getProbability_away_team_winner() > e.getProbability_home_team_winner()
+                && e.getProbability_away_team_winner() > e.getProbability_draw()) {
+            return e.getProbability_away_team_winner();
+        } else {
+            return e.getProbability_draw();
+        }
     }
 
     public Iterable<Event> list() {
