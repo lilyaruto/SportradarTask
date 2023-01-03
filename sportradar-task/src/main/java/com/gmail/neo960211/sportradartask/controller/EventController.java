710df6ca2e6c1fc2a4cc56d318589b2acf02858c
Yaroslav <ne0960211@gmail.com>
2023-01-03 13:27:19 +0100

    Launch Competitor Event with it's template

---
+++ sportradar-task/src/main/java/com/gmail/neo960211/sportradartask/controller/EventController.java
@@ -0,0 +1,28 @@
+package com.gmail.neo960211.sportradartask.controller;
+
+import com.gmail.neo960211.sportradartask.service.EventService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventController {
+
+    private EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostConstruct
+    private void loadData() {
+        eventService.sortList();
+    }
+
+    @RequestMapping("/events")
+    public String start(Model model) {
+        model.addAttribute("events", eventService.sortList());
+        return "events";
+    }
+}
--- sportradar-task/src/main/java/com/gmail/neo960211/sportradartask/service/EventService.java
+++ sportradar-task/src/main/java/com/gmail/neo960211/sportradartask/service/EventService.java
@@ -2,14 +2,21 @@ package com.gmail.neo960211.sportradartask.service;
 
 import com.gmail.neo960211.sportradartask.model.Event;
 import com.gmail.neo960211.sportradartask.repository.EventRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EventService {
     private final EventRepository eventRepository;
 
     public EventService(EventRepository eventRepository) {
         this.eventRepository = eventRepository;
+    }
+
+    public List<Event> sortList() {
+        List<Event> list = (List<Event>) eventRepository.findAll();
+        return list;
     }
 
     public Iterable<Event> list() {
